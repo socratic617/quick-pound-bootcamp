bb435387deb1de3eb4070799e662badabd8af0f4
socratic617 <erik.solis02135@gmail.com>
2023-11-19 01:24:31 -0500

    added psuedo code

--- routes/main.js
+++ routes/main.js
@@ -6,7 +6,7 @@ const postsController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
-router.get("/", homeController.getIndex);
+router.get("/", homeController.getIndex); // (endpoint, callback function)
 router.get("/profile", ensureAuth, postsController.getProfile);
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login", authController.getLogin);
